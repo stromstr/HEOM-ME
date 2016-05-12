20be37f028a634dd2b27a57357027fa711e464b8
wangyuandong <yd_wang@ruc.edu.cn>
2016-05-12 23:08:18 +0800

    db

--- src/solve_sd.f90
+++ src/solve_sd.f90
@@ -14,6 +14,8 @@ complex*16 :: do1
 !
 !construct system of linear equations in HEOM space
 !
+matr = czero
+gam  = czero
 ! confirmed!
 do n=1, bdim
   big = lmat * (n - 1)
@@ -46,24 +48,25 @@ sua(1:lmat, 1:lmat, 2, 2) = dcmplx(amsall(1:lmat, 1:lmat, 1, 2), 0.d0)   !c1_{do
 sua(1:lmat, 1:lmat, 1, 1) = conjg(transpose(sua(1:lmat, 1:lmat, 2, 1)))  !c1_{up}^{dagger}
 sua(1:lmat, 1:lmat, 1, 2) = conjg(transpose(sua(1:lmat, 1:lmat, 2, 2)))  !c1_{down}^{dagger}
 !
-![a^{\sigma}_{\alpha,\mu,\m}, rho] 
-! has bug!
+! [a^{\sigma}_{\alpha,\mu,\m}, rho] 
+! confirmed!
 do l=1, lmat
   big = bdim * (l - 1)
   do k=1, mats2
     big1 = (k - 1) * lmat**2
     do n=1, 2
+      big2 = (n - 1) * lmat * 2
       if (n .eq. 1) then
         rev = 2
       else if (n .eq. 2) then
         rev = 1
       end if
       do m=1, 2
-        big2 = lmat * (m + n - 1)
+        big3 = lmat * (m - 1)
         do i=1, lmat
           nud1 = big + i
           do j=1, lmat
-            nud2 = big + big1 + big2 + j + lmat * l
+            nud2 = big + big1 + big2 + big3 + j + lmat
             matr(nud1, nud2) = matr(nud1, nud2) + sua(i, j, rev, m)
           end do
         end do
@@ -71,25 +74,25 @@ do l=1, lmat
     end do
   end do
 end do
-!so far checked, 2016-5-11
-!
+! confirmed!
 do l=1, lmat
   big = bdim * (l - 1)
   do k=1, mats2
     big1 = (k - 1) * lmat**2
     do n=1, 2
+      big2 = (n - 1) * lmat * 2
       if (n .eq. 1) then
         rev = 2
       else if (n .eq. 2) then
         rev = 1
       end if
       do m=1, 2
-        big2 = lmat * (n + m - 1)
+        big3 = lmat * (m - 1)
         do i=1, lmat
           nud1 = i + big
           do j=1, lmat
-            big3 = (j - 1) * bdim 
-            nud2 = big1 + big2 + big3 + lmat * l + i
+            big4 = (j - 1) * bdim 
+            nud2 = big1 + big2 + big3 + big4 + lmat + i
             matr(nud1, nud2) = matr(nud1, nud2) - sua(j, l, rev, m)
           end do
         end do
@@ -98,12 +101,18 @@ do l=1, lmat
   end do
 end do
 !
-!Matsubara expansion of correlation function
-do i=1, mats
+! Matsubara expansion of correlation function
+! singular point of density function
+gam(1, 1) = wl
+gam(1, 2) = wr
+ita(1, 1) = gl * wl/(exp(eye*wl/tl) + 1)
+ita(1, 2) = gr * wr/(exp(eye*wr/tr) + 1)
+! singular point of F-D function
+do i=2, mats
   gam(i, 1) = (2 * i - 1) * pi / tl
   gam(i, 2) = (2 * i - 1) * pi / tr
-  ita(i, 1) = - eye * 2 / (tl * pi) * gl * wl**2 / (gam(i, 1)**2 + wl**2)
-  ita(i, 2) = - eye * 2 / (tr * pi) * gr * wr**2 / (gam(i, 2)**2 + wr**2)
+  ita(i, 1) = - eye * 2 / tl * gl * wl**2 / (-gam(i, 1)**2 + wl**2)
+  ita(i, 2) = - eye * 2 / tr * gr * wr**2 / (-gam(i, 2)**2 + wr**2)
 end do
 ! confirmed!
 do l=1, lmat
@@ -113,9 +122,9 @@ do l=1, lmat
     do n=1, mats
       big2 = (n - 1) * lmat**2
       do i=1, lmat
-        big2 = lmat * (i - 1)
+        big3 = lmat * (i - 1)
         do j=1, lmat
-          nud1 = big + big1 + big2 + lmat + j
+          nud1 = big + big1 + big2 + big3 + lmat + j
           matr(nud1, nud1) = matr(nud1, nud1) - eye * gam(n, k)
         end do
       end do
@@ -134,10 +143,10 @@ do l=1, lmat
         do q=1, 2
           big4 = (q - 1) * lmat
           do i=1, lmat
-            nud1 = big + big1 + big2 + big3 + big4 + lmat * l + i
+            nud1 = big + big1 + big2 + big3 + big4 + lmat + i
             do j=1, lmat
               nud2 = j
-              matr(nud1, nud2) = matr(nud1, nud2) + ita(n, k) * sua(i, j, q, m)
+              matr(nud1, nud2) = matr(nud1, nud2) + ita(n, k) * sua(i, j, m, q)
             end do
           end do
         end do
@@ -169,7 +178,7 @@ end do
 !    end do
 !  end do
 !end do
-!
+! could be bug
 do l=1, lmat
   big = (l - 1) * bdim
   do k=1, 2
@@ -181,11 +190,11 @@ do l=1, lmat
         do q=1, 2
           big4 = (q - 1) * lmat
           do i=1, lmat
-            nud1 = big + big1 + big2 + big3 + big4 + lmat * l + i
+            nud1 = big + big1 + big2 + big3 + big4 + lmat + i
             do j=1, lmat
-              big5 = (j - 1) * lmat
+              big5 = (j - 1) * bdim
               nud2 = big5 + i
-              matr(nud1, nud2) = matr(nud1, nud2) - ita(n, k) * sua(j, l, q, m)
+              matr(nud1, nud2) = matr(nud1, nud2) + conjg(ita(n, k)) * sua(j, l, m, q)
             end do
           end do
         end do
